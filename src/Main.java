ee8b6da545be0630cab471cb5102aa1b66f2ce95
Maria Malykh <marya.malykh@yandex.ru>
2022-11-09 20:36:42 +0300

    Change file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-
+        System.out.println();
+        System.out.println("Индекс массы тела: ");
         System.out.println(service.calculate(50, 1.6));
     }
 }
